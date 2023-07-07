24bb0eb0c83398a45a0311da72634adcf7a930b4
Harley John Torrisi <mail@harleycodes.com>
2023-07-07 20:52:04 +1000

    Removed Docs.

--- docs/assets/Button.stories-d3e91117.js
+++
@@ -1,6 +0,0 @@
-import{j as m}from"./jsx-runtime-91a467a5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";function l(a){const{label:n,disabled:s=!1,onClick:c}=a;return m("button",{onClick:c,disabled:s,children:n})}const d={title:"Components/Button",component:l,tags:["autodocs"]},t={args:{label:"Button"}};var o,r,e;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    label: 'Button'
-  }
-}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const b=["Primary"];export{t as Primary,b as __namedExportsOrder,d as default};
-//# sourceMappingURL=Button.stories-d3e91117.js.map
--- docs/assets/preview-deba493d.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-123ee34b.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-9004ed0f.js"),["./DocsRenderer-EYKKDMVH-9004ed0f.js","./iframe-123ee34b.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-f58e807b.js","./index-80ae7d84.js","./index-eb0d6e83.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-deba493d.js.map
